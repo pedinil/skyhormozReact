1d5ac773a98e0b150349829efbc4317d62fa8322
Pedram <pedram@Pedrams-MacBook-Pro.local>
2019-01-29 10:52:44 +0330

    adding split load in code

--- src/components/Page/Hotel/HotelInfoMain.js
+++ src/components/Page/Hotel/HotelInfoMain.js
@@ -11,66 +11,72 @@ import SearchBox from './Searchbox';
 
 
 
- class HotelInfoMain extends Component {
-     constructor(){
-         super()
-         this.state={
-             SearchH:'Hotels',
-             SearchS:'Stars',
-             Searchfield:'',
-             DataHotelInfo:DataHotelInfo
+class HotelInfoMain extends Component {
+    constructor() {
+        super()
+        this.state = {
+            SearchH: 'Hotels',
+            SearchS: 'Stars',
+            Searchfield: '',
+            DataHotelInfo: DataHotelInfo
 
-         }
-     }
-     onSearchChange=(event)=>{
-         this.setState({Searchfield:event.target.value},{SearchH:event.target.value},{SearchS:event.target.value})
-     }
-     render(){
-        const filteredHotels=this.state.DataHotelInfo.filter(DataHotelInfo=>{
-            return DataHotelInfo.HotelName.toLowerCase().includes(this.state.SearchH.toLowerCase())
-       
-});
+        }
+    }
+
+
+    onSearchChange = (event) => {
+        this.setState({ Searchfield: event.target.value })
+    }
 
     render() {
-        const filteredHotels=this.state.DataHotelInfo.filter(DataHotelInfo => {
-            return DataHotelInfo.Address.toLowerCase().includes(this.state.Searchfield.toLowerCase()),
-            
-        
+        const filteredHotels = this.state.DataHotelInfo.filter(DataHotelInfo => {
+            return DataHotelInfo.HotelName.toLowerCase().includes(this.state.SearchH.toLowerCase())
         });
-    
 
-        const Hotelinfo=filteredHotels.map((result)=>{
-            return(
+
+
+
+        {/*
+            const filteredHotels = this.state.DataHotelInfo.filter(DataHotelInfo => {
+                return DataHotelInfo.Address.toLowerCase().includes(this.state.Searchfield.toLowerCase()),
+            })
+        
+
+
+
+        const Hotelinfo = filteredHotels.map((result) => {
+            return (
                 <HotelInfo result={result} />
             )
         });
-    
+*/}
         return (
 
             <div>
-                <div  className="outer-container">
+                <div className="outer-container">
                     <HotelInfoTags />
                     <HeaderAll titlePage="Hotels" />
                     <SearchBox searchchange={this.onSearchChange} />
-                    <div className="HotelInfo">                    
-                       <div className="HotelInfo-container">
-                       
-                                    {Hotelinfo} 
-                            
+                    <div className="HotelInfo">
+                        <div className="HotelInfo-container">
 
-                            </div>
+                            {filteredHotels}
+
 
                         </div>
-                    
 
                     </div>
-                    <FooterAll />
-                    </div>
 
-               
-                    );
-                }
-            }
-            }
-            
-            export default HotelInfoMain;
+
+                </div>
+                <FooterAll />
+            </div>
+
+
+        );
+    }
+}
+
+
+
+export default HotelInfoMain;
--- src/components/Page/Hotel/Searchbox.js
+++ src/components/Page/Hotel/Searchbox.js
@@ -1,57 +1,57 @@
 import React from 'react'
 
-const SearchBox=({ Searchfield , searchchange })=>{
-    return(
+const SearchBox = ({ Searchfield, searchchange }) => {
+    return (
         <div>
-        <div class="search">
-            <div class="col-md-10 col-md-offset-1">
-              <div class="form-section">
-                      <div class="col-md-4">
-                        
-                          <label class="sr-only" >Location</label>
-                          <input type="text" className="form-control" onChange={searchchange}  placeholder="Location" />
-                     
-                      </div>
-                      <div class="Hotel-Names col-md-2">
-                        
-                          <label class="sr-only" >Hotel-Names</label>
-                          <select class="form-control" onChange={searchchange}  placeholder="Select Hotel" >
-                              <option value="" disabled selected>Hotels</option>
-                              <option value="1">Hotel1</option>
-                              <option value="H2">H2</option>
-                              <option value="H3">H3</option>
-                              <option value="H4">H4</option>
-                              <option value="H5">H5</option>
-                              </select>
+            <div class="search">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="form-section">
+                        <div class="col-md-4">
 
-                      </div>
-                      <div class="Stars-number col-md-2">
-                          <label class="sr-only" >Stars</label>
-                          <select name="Stars-number" class="form-control" onChange={searchchange}>
-                              <option value=""disabled selected>Stars</option>
-                              <option value="1">1</option>
-                              <option value="2">2</option>
-                              <option value="3">3</option>
-                              <option value="4">4</option>
-                              <option value="5">5</option>
-                              </select>
+                            <label class="sr-only" >Location</label>
+                            <input type="text" className="form-control" onChange={searchchange} placeholder="Location" />
 
-                      </div>
-                      
-                      
-                     
-                      
-                    
-                
-              </div>
-            
-          </div>
-        
-      </div>    
+                        </div>
+                        <div class="Hotel-Names col-md-2">
 
-        
-    
-    </div>
+                            <label class="sr-only" >Hotel-Names</label>
+                            <select class="form-control" onChange={searchchange} placeholder="Select Hotel" >
+                                <option value="" disabled selected>Hotels</option>
+                                <option value="1">Hotel1</option>
+                                <option value="H2">H2</option>
+                                <option value="H3">H3</option>
+                                <option value="H4">H4</option>
+                                <option value="H5">H5</option>
+                            </select>
+
+                        </div>
+                        <div class="Stars-number col-md-2">
+                            <label class="sr-only" >Stars</label>
+                            <select name="Stars-number" class="form-control" onChange={searchchange}>
+                                <option value="" disabled selected>Stars</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select>
+
+                        </div>
+
+
+
+
+
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+        </div>
     );
 
 }
